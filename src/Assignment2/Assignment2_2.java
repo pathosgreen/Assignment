44bf5ce0f87c08325fdd551253ca313434dfab88
pathosgreen <hexun_@naver.com>
2022-05-10 20:25:07 +0900

    2022_05_10

---
+++ src/Assignment2/Assignment2_2.java
@@ -0,0 +1,24 @@
+package Assignment2;
+
+import java.util.Scanner;
+
+public class Assignment2_2 {
+	public static void main(String[] args) {
+		// 읽은 정수값의 차
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print(" 정수 A : ");
+		int a = scanner.nextInt();
+		System.out.print(" 정수 B : ");
+		int b = scanner.nextInt();
+		
+		int diff;
+		if( a > b ) {
+			diff = a-b;
+		}else {
+			diff = b-a;
+		}
+		System.out.println("값의 차이 : " + diff);
+		
+	}
+}
---
+++ src/Assignment2/Assignment2_6.java
@@ -0,0 +1,29 @@
+package Assignment2;
+
+import java.util.Scanner;
+
+public class Assignment2_6 {
+	public static void main(String[] args) {
+		// 두 정숫값중 작은값과 큰값
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(" 정수 A : ");
+		int a = scanner.nextInt();
+		System.out.print(" 정수 B : ");
+		int b = scanner.nextInt();
+
+		if( a == b ) {
+			System.out.println(" 둘이 같음.");
+		}else{
+			int max,min;
+			if(a>b){
+				max = a;
+				min = b;
+			}else {
+				max = b;
+				min = a;
+			}
+			System.out.println(" 최댓값 : " + max);
+			System.out.println(" 최솟값 : " + min);
+		}
+	}
+}
