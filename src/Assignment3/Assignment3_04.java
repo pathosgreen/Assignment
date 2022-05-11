ffbe7fce1557deab0915803a4e550551275dec63
pathosgreen <hexun_@naver.com>
2022-05-11 19:36:10 +0900

    2022_05_11 숙제 다시올림

---
+++ src/Assignment3/Assignment3_04.java
@@ -0,0 +1,22 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class Assignment3_04 {
+	public static void main(String[] args) {
+		// 두개의 정숫값 받아 그 사이 모든값을 작은것부터 표시
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자 a : "); int a = scanner.nextInt();
+		System.out.print("숫자 b : "); int b = scanner.nextInt();
+		
+		if(a > b) {
+			int temp = a;
+			a=b;
+			b=temp;
+		}
+		for(int i=a+1; i<b; i++) {
+			System.out.print(i + " ");
+		}
+		
+	}
+}
