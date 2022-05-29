9e0842fac61a4e44227a25dc3945f543eb0e2912
pathosgreen <hexun_@naver.com>
2022-05-29 19:12:54 +0900

    2022_05_28~29 과제

---
+++ src/Assignmnet1_15/_1/IdTester.java
@@ -0,0 +1,17 @@
+package Assignmnet1_15._1;
+// Ver.1
+public class IdTester {
+	public static void main(String[] args) {
+		
+		Id a = new Id();
+		Id b = new Id();
+		
+		System.out.println("a의 식별번호 : "+ a.getId());
+		System.out.println("b의 식별번호 : "+ b.getId());
+		
+		System.out.println("Id.counter = "+Id.counter);
+		System.out.println("a.counter = "+a.counter);
+		System.out.println("b.counter = "+b.counter);
+	}
+	
+}
---
+++ src/Assignmnet1_15/_2/Id.java
@@ -0,0 +1,17 @@
+package Assignmnet1_15._2;
+// Ver.2
+public class Id {
+	
+	private static int counter = 0;
+	private int id;
+	
+	public Id() {
+		id = ++counter;
+	}
+
+	public int getId() {return id;}
+	
+	public static int getMaxId() {
+		return counter;
+	}
+}
